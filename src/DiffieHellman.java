0cbd913220080a383626773236d054f77d538e84
Cyrois <cyrois3@gmail.com>
2015-10-17 15:53:57 -0700

    DH stuff

---
+++ src/DiffieHellman.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+import java.util.Random;
+
+public class DiffieHellman {
+
+	public static BigInteger generateRandomSecretValue() {
+		return generate1024BitNum();
+	}
+	
+	public static BigInteger generateBigIntPrime() {
+		return new BigInteger(1024, Integer.MAX_VALUE, new Random());
+	}
+	
+	public static BigInteger generateBigIntG() {
+		return generate1024BitNum();
+	}
+
+	public BigInteger dhMod(BigInteger g, BigInteger n, BigInteger p) {
+		return g.modPow(n, p);
+	}
+	
+	private static BigInteger generate1024BitNum() {
+		return new BigInteger(1024, new Random());
+	}
+}
